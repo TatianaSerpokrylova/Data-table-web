94c2424e2e3b0b9198064463cb76477dd4973782
Татьяна Серпокрылова <tatanaserpokrylova@Noutbuk-Tatana.local>
2023-05-08 17:57:58 +0400

    Rename header Customers

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./AppHeader.css";
 
 const AppHeader = () => {
-  return <h1 className="app-header">Customers</h1>;
+  return <h1 className="app-header">Customers of shop</h1>;
 };
 
 export default AppHeader;
