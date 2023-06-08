476c4cc2cc200a149b7e2c37410c463665dd3085
Татьяна Серпокрылова <tatanaserpokrylova@Noutbuk-Tatana.local>
2023-06-08 23:27:37 +0400

    changes in .eslintrc and .babelrc

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,8 +1,8 @@
-import React from "react";
-import "./AppHeader.css";
+import React from 'react';
+import './AppHeader.css';
 
 const AppHeader = () => {
-  return <h1 className="app-header">Customers of shop</h1>;
+  return <h1 className='app-header'>Customers of shop</h1>;
 };
 
 export default AppHeader;
